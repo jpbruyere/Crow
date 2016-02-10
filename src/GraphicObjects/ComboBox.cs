b8c22d34279d7919620f295107054ba53b17034b
jpbruyere <jp.bruyere@hotmail.com>
2016-02-10 11:01:44 +0100

    clean using

--- src/GraphicObjects/ComboBox.cs
+++ src/GraphicObjects/ComboBox.cs
@@ -1,23 +1,5 @@
 ﻿using System;
 
-
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-//using OpenTK.Graphics.OpenGL;
-
-using Cairo;
-
-using winColors = System.Drawing.Color;
-using System.Diagnostics;
-using System.Xml.Serialization;
-using OpenTK.Input;
-using System.ComponentModel;
-using System.Xml;
-using System.IO;
-using System.Collections;
-using System.Threading;
-
 namespace Crow
 {
 	[DefaultTemplate("#Crow.Templates.ComboBox.goml")]
