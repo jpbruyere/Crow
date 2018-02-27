19944e8637bdec5a873c8a5c690973d9ff134c6d
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2018-02-28 00:51:17 +0100

    change styling grammar for value now enclose in double quote

---
+++ src/ParsingException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Crow.Coding
+{
+	public class ParserException : Exception
+	{
+		public int Line;
+		public int Column;
+		public ParserException(int line, int column, string txt)
+			: base(string.Format("Parser exception ({0},{1}): {2}", line, column, txt))
+		{
+			Line = line;
+			Column = column;
+		}
+		public ParserException(int line, int column, string txt, Exception innerException)
+			: base(string.Format("Parser exception ({0},{1}): {2}", line, column, txt), innerException)
+		{}
+	}
+}
+
