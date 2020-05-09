f4d2a94b44a5176954a24bef67eba84be78f6b91
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2020-05-09 15:07:52 +0200

    source tree organization

---
+++ Crow/src/EventArgs/ListChangedEventArg.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2020  Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
+//
+// This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+using System;
+namespace Crow
+{
+	public class ListChangedEventArg : EventArgs
+	{
+		public int Index;
+		public object Element;
+		public ListChangedEventArg (int index, object element)
+		{
+			Index = index;
+			Element = element;
+		}
+	}
+}
---
+++ Crow/src/IObservableList.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2013-2020  Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
+//
+// This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+
+using System;
+
+namespace Crow
+{
+	public interface IObservableList {
+		event EventHandler<ListChangedEventArg> ListAdd;
+		event EventHandler<ListChangedEventArg> ListRemove;
+		event EventHandler<ListChangedEventArg> ListEdit;
+
+		void Insert ();
+		void Remove ();
+		void RaiseEdit ();
+	}
+}
+
