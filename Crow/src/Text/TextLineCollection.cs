57840187ad0be6f2cb1747aa67f40fdadd93d4a2
Jean-Philippe Bruyère <jp_bruyere@hotmail.com>
2021-02-06 17:55:00 +0100

    TextLineCollection.update(TextChange ...)

--- Crow/src/Text/TextLineCollection.cs
+++ Crow/src/Text/TextLineCollection.cs
@@ -24,8 +24,91 @@ namespace Crow.Text
             
             length = _lines.Length;
         }
+        
+        public LineCollection (string _text, int capacity = 4) : this (capacity) {
+            Update (_text.AsSpan ());
+        }
         #endregion
 
+        public void Update (ReadOnlySpan<char> _text) {
+            length = 0;
+            int start = 0, i = 0;
+            while (i < _text.Length) {
+                char c = _text[i];
+                if (c == '\r') {
+                    if (++i < _text.Length) {
+                        if (_text[i] == '\n')
+                            Add (new TextLine (start, i - 1, ++i));
+                        else
+                            Add (new TextLine (start, i - 1, i));
+                    } else
+                        Add (new TextLine (start, i - 1, i));
+                    start = i;
+                } else if (c == '\n') {
+                    if (++i < _text.Length) {
+                        if (_text[i] == '\r')
+                            Add (new TextLine (start, i - 1, ++i));
+                        else
+                            Add (new TextLine (start, i - 1, i));
+                    } else
+                        Add (new TextLine (start, i - 1, i));
+                    start = i;
+
+                } else if (c == '\u0085' || c == '\u2028' || c == '\u2029')
+                    Add (new TextLine (start, i - 1, i));
+                else
+                    i++;
+            }
+
+            if (start < i)
+                Add (new TextLine (start, _text.Length, _text.Length));
+            else
+                Add (new TextLine (_text.Length, _text.Length, _text.Length));
+        }
+
+        public void Update (TextChange change) {
+            CharLocation locStart = GetLocation (change.Start);
+            int charsDiff = change.ChangedText.Length - change.Length;
+            int lineEnd = locStart.Line;
+            while (lineEnd < length - 1 && change.End >= lines[lineEnd + 1].Start)
+                lineEnd++;
+            int columnEnd = change.End - lines[lineEnd].Start;
+            int lineEndLineBreakLength = lines[lineEnd].LineBreakLength;
+
+            LineCollection newLines = new LineCollection (change.ChangedText);
+            int linesDiff = newLines.length - 1 - (lineEnd - locStart.Line);
+            TextLine endTl = lines[lineEnd];
+
+            if (linesDiff < 0)
+                RemoveAt (locStart.Line + 1, -linesDiff);
+            else if (linesDiff > 0) {
+                for (int i = 0; i < linesDiff; i++)
+                    Insert (locStart.Line + 1, default);
+            }
+
+            int remainingColumns = endTl.Length - columnEnd;
+            lineEnd += linesDiff;
+            lines[lineEnd].SetLength (0);
+            lines[locStart.Line].SetLength (locStart.Column + newLines[0].Length);
+            lines[lineEnd].Length += remainingColumns;
+            if (newLines.Count > 1) {
+                lines[lineEnd].Length += newLines[newLines.Count - 1].Length;                
+                lines[locStart.Line].LengthIncludingLineBreak = lines[locStart.Line].Length + newLines[0].LineBreakLength;
+            }
+            lines[lineEnd].LengthIncludingLineBreak = lines[lineEnd].Length + endTl.LineBreakLength;
+
+            for (int i = 1; i < newLines.Count - 1; i++) {
+                int l = locStart.Line + i;
+                lines[l] = newLines[i];
+                lines[l].Start = lines[l - 1].EndIncludingLineBreak;
+            }
+            if (lineEnd > 0)
+                lines[lineEnd].Start = lines[lineEnd - 1].EndIncludingLineBreak;
+
+            //shift start for remaining lines
+            for (int i = lineEnd + 1; i < length; i++)
+                lines[i].Start += charsDiff;            
+        }
         public int GetAbsolutePosition (CharLocation loc) => lines[loc.Line].Start + loc.Column;        
         public CharLocation GetLocation (int absolutePosition) {
             TextLine tl = new TextLine (absolutePosition);
@@ -76,26 +159,39 @@ namespace Crow.Text
             length--;
             return true;
         }
-        public IEnumerator<TextLine> GetEnumerator () => new LineEnumerator (this);
+        public IEnumerator<TextLine> GetEnumerator () => new Enumerator (this);
         IEnumerator IEnumerable.GetEnumerator () => GetEnumerator ();
 
-        public int IndexOf (TextLine item) {
-            throw new NotImplementedException ();
-        }
+        public int IndexOf (TextLine item) => Array.IndexOf (lines, item);
 
         public void Insert (int index, TextLine item) {
-            throw new NotImplementedException ();
+            if (lines.Length < length + 1) {
+                TextLine[] tmp = new TextLine[length * 2];
+                lines.AsSpan (0, index).CopyTo (tmp);
+                lines.AsSpan (index).CopyTo (tmp.AsSpan (index + 1));
+                lines = tmp;
+            }else
+                lines.AsSpan (index, length - index).CopyTo (lines.AsSpan (index + 1));            
+            lines[index] = item;
+            length++;
         }
 
         public void RemoveAt (int index) {
-            throw new NotImplementedException ();
+            if (index + 1 < length)
+                lines.AsSpan (index + 1, length - index - 1).CopyTo (lines.AsSpan (index));
+            length--;
+        }
+        public void RemoveAt (int index, int count) {
+            if (index + count < length)
+                lines.AsSpan (index + count, length - index - count).CopyTo (lines.AsSpan (index));
+            length -= count;
         }
 
-        public class LineEnumerator : IEnumerator<TextLine>
+        public class Enumerator : IEnumerator<TextLine>
         {
             TextLine[] lines;
             int length, position = -1;
-            public LineEnumerator (LineCollection coll) {
+            public Enumerator (LineCollection coll) {
                 lines = coll.lines;
                 length = coll.length;
             }
